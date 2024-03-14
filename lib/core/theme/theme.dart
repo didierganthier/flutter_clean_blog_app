897290f393c30ed217f9ddbc52bb9e866727d0b5
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-03-14 02:04:17 -0400

    Edit scaffoldBackgroundColor

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_blog_app/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark();
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+  );
 }
