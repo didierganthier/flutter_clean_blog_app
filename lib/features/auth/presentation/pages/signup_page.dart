df9e7fb66c2dd83267962879842654b80bf50e13
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-03-14 02:47:33 -0400

    Create AuthField widget

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_blog_app/features/auth/presentation/widgets/auth_field.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -10,6 +11,27 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Password'),
+          ],
+        ),
+      ),
+    );
   }
 }
