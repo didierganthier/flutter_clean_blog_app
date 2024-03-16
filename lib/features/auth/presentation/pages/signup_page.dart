c40839a7924834b8fe2e874e3ef0b4a5a2ee23a4
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-03-16 06:48:04 -0400

    Add const modifiers

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -19,18 +19,18 @@ class _SignupPageState extends State<SignupPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Sign Up.',
               style: TextStyle(
                 fontSize: 50,
               ),
             ),
             const SizedBox(height: 30),
-            AuthField(hintText: 'Name'),
+            const AuthField(hintText: 'Name'),
             const SizedBox(height: 15),
-            AuthField(hintText: 'Email'),
+            const AuthField(hintText: 'Email'),
             const SizedBox(height: 15),
-            AuthField(hintText: 'Password'),
+            const AuthField(hintText: 'Password'),
             const SizedBox(height: 20),
             const AuthGradientButton(),
             const SizedBox(height: 20),
