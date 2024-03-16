3c8ce985abc0ff6f69eeece84e49decb093cc3b1
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-03-16 07:00:42 -0400

    Add controller to AuthField

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
+  final TextEditingController controller;
 
-  const AuthField({Key? key, required this.hintText}) : super(key: key);
+  const AuthField({Key? key, required this.hintText, required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
       ),
