053ac1e3eb8be1e9a788eee458dde6f933ec4961
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-03-16 07:10:56 -0400

    Add obscureText condition

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 class AuthField extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
+  final bool isObscureText;
 
-  const AuthField({Key? key, required this.hintText, required this.controller})
-      : super(key: key);
+  const AuthField({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      obscureText: isObscureText,
       decoration: InputDecoration(
         hintText: hintText,
       ),
