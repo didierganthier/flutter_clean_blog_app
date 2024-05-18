4e124d0fa09cd63f4e4c7742f7c8bcfc3234fd08
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-05-18 18:42:18 -0400

    Add methods to AuthRepository

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,1 +1,15 @@
-abstract interface class AuthRepository {}
+import 'package:flutter_clean_blog_app/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signupWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, String>> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
