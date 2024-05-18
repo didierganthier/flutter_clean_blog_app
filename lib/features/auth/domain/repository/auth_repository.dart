6d1d1f152a120b84a3d9f3769e1821088cc05de8
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-05-18 18:44:41 -0400

    Add comments to improve documentation

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,13 +1,21 @@
 import 'package:flutter_clean_blog_app/core/error/failure.dart';
 import 'package:fpdart/fpdart.dart';
 
+/// Interface for the authentication repository.
+/// Provides methods for user signup and login.
 abstract interface class AuthRepository {
-  Future<Either<Failure, String>> signupWithEmailAndPassword({
+  /// Signs up a user with the provided [name], [email], and [password].
+  /// Returns an [Either] with [Failure] in case of an error or a [String] representing
+  /// a user ID or token upon successful signup.
+  Future<Either<Failure, String>> signUpWithEmailAndPassword({
     required String name,
     required String email,
     required String password,
   });
 
+  /// Logs in a user with the provided [email] and [password].
+  /// Returns an [Either] with [Failure] in case of an error or a [String] representing
+  /// a user ID or token upon successful login.
   Future<Either<Failure, String>> loginWithEmailAndPassword({
     required String email,
     required String password,
