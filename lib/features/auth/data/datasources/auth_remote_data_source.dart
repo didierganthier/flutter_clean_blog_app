333b254694c3221f2d44b500c1866a93ad2a5bba
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-05-23 14:59:12 -0400

    Create AuthRemoteDataSource

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,13 @@
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImplementation {}
