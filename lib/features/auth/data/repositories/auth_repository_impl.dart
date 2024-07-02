38ab0b9becc8e6f735be9c92bb9b541da6d8fd21
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-07-02 11:57:22 -0400

    Implement signUpWithEmailAndPassword method

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_clean_blog_app/core/error/exceptions.dart';
 import 'package:flutter_clean_blog_app/core/error/failures.dart';
 import 'package:flutter_clean_blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:flutter_clean_blog_app/features/auth/domain/repository/auth_repository.dart';
@@ -17,8 +18,19 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<Either<Failure, String>> signUpWithEmailAndPassword(
-      {required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailAndPassword
-    throw UnimplementedError();
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      return Right(userId);
+    } on ServerException catch (e) {
+      return Left(Failure(e.message));
+    }
   }
 }
